b5d63115a23ed97631cc36380116ab9edf4892ea
Ansuya Chauhan <ansuya.chauhan@gmail.com>
2021-04-07 22:52:47 -0500

    add projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,7 +126,7 @@
   }
 
   // Real view height for mobile devices
-  if (window.matchMedia("(max-width: 767px)").matches) {
+  if (window.matchMedia("(max-width: 900px)").matches) {
     $('#hero').css({
       height: $(window).height()
     });
@@ -146,6 +146,14 @@
     $(this).find('p, .btn-menu, .btn-book').addClass('animate__animated animate__fadeInUp');
   });
 
+
+  var section = $("#section");
+  section( function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    
+  });
+ 
+  // Back to top button
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -195,6 +203,56 @@ $("membership.box").click(function() {
       location.href = jQuery(this).val(1);
   })
 });
+
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+  
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 4;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
+
+
+$('#carouselExample').carousel({ 
+              interval: 2000
+      });
+
+
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+  $('a.thumb').click(function(event){
+    event.preventDefault();
+    var content = $('.modal-body');
+    content.empty();
+      var title = $(this).attr("title");
+      $('.modal-title').html(title);        
+      content.html($(this).html());
+      $(".modal-profile").modal({show:true});
+  });
+
+});
+
+var styleSwiper = new StyleSwiper({
+  debug:true,
+  el:document.getElementById("myStyleSwiper")
+});
+
+
 
 
 
